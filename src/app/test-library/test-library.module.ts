740917aba0ff524b9ac5010d0f41e4e84de236e2
JayChase <JonathanChase@outlook.com>
2017-08-01 21:58:18 +0900

    start

---
+++ src/app/test-library/test-library.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    TestComponent
+  ],
+  exports: [
+    TestComponent
+  ]
+})
+export class TestLibraryModule { }
